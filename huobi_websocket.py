69933fbd791a6e065c4807cd72e44eadd43da657
huan-wang5 <huan-wang5@hnair.com>
2018-06-20 17:36:22 +0800

    okcoin websocket测试

--- huobi_websocket.py
+++ huobi_websocket.py
@@ -1,24 +1,24 @@
 import gzip
 import json
-import pprint
 import threading
 
 import time
 import websocket
+from datetime import datetime
 
 
 def send_message(ws, message_dict):
     data = json.dumps(message_dict).encode()
     print("Sending Message:")
-    pprint.pprint(message_dict)
+    print(message_dict)
     ws.send(data)
 
 
 def on_message(ws, message):
     unzipped_data = gzip.decompress(message).decode()
     msg_dict = json.loads(unzipped_data)
-    print("Recieved Message: ")
-    pprint.pprint(msg_dict)
+    print("Recieved Message: ", datetime.now())
+    print(msg_dict)
     if 'ping' in msg_dict:
         data = {
             "pong": msg_dict['ping']
