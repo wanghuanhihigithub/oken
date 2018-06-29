a8dc12874a5cbd850766296837e8bec563559584
huan-wang5 <huan-wang5@hnair.com>
2018-06-30 00:16:18 +0800

    1111

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 import fcoin
 import time
 
-api = fcoin.authorize('key', 'secret', int(round(time.time() * 1000)))
+api = fcoin.authorize('key', 'secret')
 t = api.market.get_ticker("ethbtc")
 print(t)
