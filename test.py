ec533c6cb6672d0d99662fe28ef6d777ffea38c0
huan-wang5 <huan-wang5@hnair.com>
2018-06-30 12:37:14 +0800

    修改

--- test.py
+++
@@ -1,6 +0,0 @@
-import fcoin
-
-api = fcoin.authorize('dcda9f2b36584b7d81f9eeb7f3465e7e', '78bff669d4f24c2f85f17c0b71c52f37')
-t = api.get_ticker("ethbtc")
-#t = api.market.get_ticker("ethbtc")
-print(t)
