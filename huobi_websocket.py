66212b0dc3d70eaba0f90e684945a957d1b6cfed
huan-wang5 <huan-wang5@hnair.com>
2018-06-30 10:01:27 +0800

    修改

--- huobi_websocket.py
+++ huobi_websocket.py
@@ -41,15 +41,6 @@ def on_error(ws, error):
 
 def on_close(ws):
     print("### closed ###")
-    websocket.enableTrace(True)
-    ws = websocket.WebSocketApp(
-        "wss://api.huobi.pro/ws",
-        on_open=on_open,
-        on_message=on_message,
-        on_error=on_error,
-        on_close=on_close
-    )
-    ws.run_forever()
 
 
 def on_open(ws):
