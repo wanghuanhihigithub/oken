7cf3f84222ce70b20db2f8c2f3dfb535e81c7074
huan-wang5 <huan-wang5@hnair.com>
2018-06-30 18:03:48 +0800

    1111

--- handlers.py
+++ handlers.py
@@ -22,11 +22,16 @@ def api_getCoinsVs():
    return getFromVsTo()
 
 def getFromVsTo():
+    now = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    conn = redis.Redis(host='127.0.0.1', port=6379, db=0)
+    if(conn.get(now) != None):
+        return conn.get(now)
     trade_url = "https://www.okex.com/v2/futures/market/indexTicker?symbol=f_usd_btc"
     r = requests.get(trade_url)
     if (r.status_code == 200):
         text = json.loads(r.text)
-        text["createdTime"] = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        text["createdTime"] = now
+        conn.set(now, text)
         return text
 
 @get("/api/huobiCoinsVs")
