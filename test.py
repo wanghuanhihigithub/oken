252d7b13e5b48cc24c3ed61cf004e67e66ee8da2
huan-wang5 <huan-wang5@hnair.com>
2018-06-30 19:22:33 +0800

    1111

--- test.py
+++ test.py
@@ -12,35 +12,20 @@ def api_getCoinsVs():
 def getFromVsTo():
     now = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
     #conn = redis.Redis(host='127.0.0.1', port=6379, db=0)
-    conn = get_redis_connect()
-    if(conn.exists(now)):
-       return conn.get(now)
+    #if(conn.exists(now)):
+       #return conn.get(now)
     trade_url = "https://www.okex.com/v2/futures/market/indexTicker?symbol=f_usd_btc"
     r = requests.get(trade_url)
     if (r.status_code == 200):
         text = json.loads(r.text)
         now = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
         text["createdTime"] = now
-        conn.set(now, text)
+        #conn.set(now, text)
         return text
 
 @route("/api/huobiCoinsVs")
 def api_getHuobiCoinsVs():
     #conn = redis.Redis(host='127.0.0.1', port=6379, db=0)
-    conn = get_redis_connect()
-    return conn.get("usdt-btc")
+    #return conn.get("usdt-btc")
+    return 11
 
-@route('/')
-def index(name='World'):
-    return '&lt;b&gt;Hello %s!&lt;/b&gt;' % name
-
-def get_redis_connect():
-    redis_config = {
-    'host': 'localhost',
-    'port': 6379
-    }
-    pool = redis.ConnectionPool(**redis_config)
-    r = redis.Redis(connection_pool=pool)
-    return r
-
-run(host='localhost', port=9000)
