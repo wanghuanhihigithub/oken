0912cca1f01da074addd889f9a37dcc63b70825a
huan-wang5 <huan-wang5@hnair.com>
2018-06-30 13:19:52 +0800

    111

--- app.py
+++ app.py
@@ -9,39 +9,12 @@ async web application.
 
 import logging; logging.basicConfig(level=logging.INFO)
 
-import asyncio, os, json, time
-from datetime import datetime
+import asyncio, json
 
 from aiohttp import web
-from jinja2 import Environment, FileSystemLoader
 
-from config import configs
 
-import orm
-from coroweb import add_routes, add_static
-
-from handlers import COOKIE_NAME
-
-def init_jinja2(app, **kw):
-    logging.info('init jinja2...')
-    options = dict(
-        autoescape = kw.get('autoescape', True),
-        block_start_string = kw.get('block_start_string', '{%'),
-        block_end_string = kw.get('block_end_string', '%}'),
-        variable_start_string = kw.get('variable_start_string', '{{'),
-        variable_end_string = kw.get('variable_end_string', '}}'),
-        auto_reload = kw.get('auto_reload', True)
-    )
-    path = kw.get('path', None)
-    if path is None:
-        path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'templates')
-    logging.info('set jinja2 template path: %s' % path)
-    env = Environment(loader=FileSystemLoader(path), **options)
-    filters = kw.get('filters', None)
-    if filters is not None:
-        for name, f in filters.items():
-            env.filters[name] = f
-    app['__templating__'] = env
+from coroweb import add_routes
 
 @asyncio.coroutine
 def logger_factory(app, handler):
@@ -51,23 +24,6 @@ def logger_factory(app, handler):
         # yield from asyncio.sleep(0.3)
         return (yield from handler(request))
     return logger
-
-@asyncio.coroutine
-def auth_factory(app, handler):
-    @asyncio.coroutine
-    def auth(request):
-        logging.info('check user: %s %s' % (request.method, request.path))
-        request.__user__ = None
-        cookie_str = request.cookies.get(COOKIE_NAME)
-        if cookie_str:
-            user = yield from cookie2user(cookie_str)
-            if user:
-                logging.info('set current user: %s' % user.email)
-                request.__user__ = user
-        if request.path.startswith('/manage/') and (request.__user__ is None or not request.__user__.admin):
-            return web.HTTPFound('/signin')
-        return (yield from handler(request))
-    return auth
 
 @asyncio.coroutine
 def data_factory(app, handler):
@@ -124,24 +80,10 @@ def response_factory(app, handler):
         return resp
     return response
 
-def datetime_filter(t):
-    delta = int(time.time() - t)
-    if delta < 60:
-        return u'1分钟前'
-    if delta < 3600:
-        return u'%s分钟前' % (delta // 60)
-    if delta < 86400:
-        return u'%s小时前' % (delta // 3600)
-    if delta < 604800:
-        return u'%s天前' % (delta // 86400)
-    dt = datetime.fromtimestamp(t)
-    return u'%s年%s月%s日' % (dt.year, dt.month, dt.day)
-
 @asyncio.coroutine
 def init(loop):
-    yield from orm.create_pool(loop=loop, **configs.db)
     app = web.Application(loop=loop, middlewares=[
-        logger_factory, auth_factory, response_factory
+        logger_factory, response_factory
     ])
     add_routes(app, 'handlers')
     srv = yield from loop.create_server(app.make_handler(), '127.0.0.1', 9000)
