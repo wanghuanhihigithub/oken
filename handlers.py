cf9dfbda524b9120bea842d9d81a31e3d9151745
huan-wang5 <huan-wang5@hnair.com>
2018-06-30 10:48:37 +0800

    修改

--- handlers.py
+++ handlers.py
@@ -1,13 +1,15 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+
+
 __author__ = 'Wang Huan'
 
 ' url handlers '
 
 from coroweb import get, post
 
-from models import CoinProfit,SysSetting, SmsLog
+from models import CoinProfit, SysSetting, SmsLog
 from config import configs
 import requests
 import json
@@ -25,7 +27,7 @@ _COOKIE_KEY = configs.session.secret
 
 #全局的header
 headers = {
-    'authorization': 'eyJhbGciOiJIUzUxMiJ9.eyJqdGkiOiI4MzEzMzExOC1mZmE4LTRhNmUtODEzYS1hOTg4Y2FjMDVlNWJYQ2tOIiwidWlkIjoiM2xuNzNkSmxtNXJhQzZIK0RtWG9Rdz09Iiwic3ViIjoiMTg5KioqODQ5MCIsInN0YSI6MCwibWlkIjowLCJpYXQiOjE1Mjg4NTU0MTYsImV4cCI6MTUyOTQ2MDIxNiwiaXNzIjoib2tjb2luIn0.oUbH0k5-0vY5jehWg1F7KtvIcDCo8o-274LNBYvYCL1sfV_QWW7eDVI-qq3u5MyxPl9ixoPuXG3Ckzq1NWWnfA',
+    'authorization': 'eyJhbGciOiJIUzUxMiJ9.eyJqdGkiOiIxMDA1OTIzZi05ODVmLTQyMDItYjNjYi1mMzA5ZjcxZWYxNjlXeUt4IiwidWlkIjoiM2xuNzNkSmxtNXJhQzZIK0RtWG9Rdz09Iiwic3ViIjoiMTg5KioqODQ5MCIsInN0YSI6MCwibWlkIjowLCJpYXQiOjE1MzAzMjY4NTMsImV4cCI6MTUzMDkzMTY1MywiYmlkIjowLCJpc3MiOiJva2NvaW4ifQ.lj-YqBqo6R8sfGzaYQ0n5GINdAh7gHX-h_q9959PMHfBWCIhsuvD_z48X3PCha6gPYu-E1Q4-CLEFSYXy__49A',
     'content-type': 'application/json',
     'cookie': '__cfduid=d268f871c1f8ddf6683c1293165f5ee831525785471; locale=zh_CN; _ga=GA1.2.1105539038.1525785471; first_ref=https://www.okex.com/account/login.htm; perm=85E2BB8DDAF5EC8B4DAB9C6429D20733; _gid=GA1.2.1414655093.1526568686; Hm_lvt_b4e1f9d04a77cfd5db302bc2bcc6fe45=1525785472,1526568686,1526644458; isLogin=1; product=btc_usdt; lp=/future/trade; Hm_lpvt_b4e1f9d04a77cfd5db302bc2bcc6fe45=1526649066; ref=https://www.okex.com/futureTrade/beforeFuture; _gat_gtag_UA_115738092_1=1',
     'referer': 'https://www.okex.com/fiat/c2c',
