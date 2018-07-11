2e13952bc00e10f3cadfc0eb8bbafa31a93c4957
huan-wang5 <huan-wang5@hnair.com>
2018-07-11 09:06:40 +0800

    111

--- app2.py
+++ app2.py
@@ -8,43 +8,33 @@ from fcoin import Fcoin
 
 @route("/api/coinsVs")
 def api_getCoinsVs():
-
-    try:
         return getFromVsTo()
-    except Exception as e:
-        print("获取oken币异常" + e)
 
 @route("/api/huobiCoinsVs")
 def api_getHuobiCoinsVs():
-    try:
-        conn = redis.Redis(host='127.0.0.1', port=6379, db=0)
-        return conn.get("usdt-btc")
-    except Exception as e:
-        print("获取火币异常" + e)
+    conn = redis.Redis(host='127.0.0.1', port=6379, db=0)
+    return conn.get("usdt-btc")
+
 
 @route("/api/fcoinVs")
 def api_getFcoinVs():
-    try:
-        fcoin = Fcoin(api_key, api_secret)
-        ticket = fcoin.get_ticket("btcusdt")
-        now = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-        ticket["createdTime"] = now
-        return ticket
-    except Exception as e:
-        print("获取fcoin异常" + e)
+    fcoin = Fcoin(api_key, api_secret)
+    ticket = fcoin.get_ticket("btcusdt")
+    now = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    ticket["createdTime"] = now
+    return ticket
+
 
 @route("/api/coinEx")
 def api_getCoinEx():
-    try:
-        trade_url = "https://api.coinex.com/v1/market/ticker?market=btcusdt"
-        r = requests.get(trade_url, timeout=2)
-        if (r.status_code == 200):
-            text = json.loads(r.text)
-            now = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-            text["createdTime"] = now
-            return text
-    except Exception as e:
-        print("获取coinEx异常" + e)
+    trade_url = "https://api.coinex.com/v1/market/ticker?market=btcusdt"
+    r = requests.get(trade_url, timeout=2)
+    if (r.status_code == 200):
+        text = json.loads(r.text)
+        now = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        text["createdTime"] = now
+        return text
+
 
 def getFromVsTo():
     trade_url = "https://www.okex.com/api/v1/ticker.do?symbol=btc_usdt"
