c2c9587a9d2317e0072705468334060b7937e724
huan-wang5 <huan-wang5@hnair.com>
2018-07-12 19:50:12 +0800

    111

--- oken-websocket.py
+++ oken-websocket.py
@@ -1,39 +1,52 @@
+import gzip
+import json
+import threading
 
 import websocket
-import sys
-import zlib
+from datetime import datetime
+import redis
+import time
 
-def on_open(self):
-    # subscribe okcoin.com spot ticker
-    self.send({'event':'addChannel','channel':'ok_sub_spot_usd_btc_ticker','binary':0})
+def send_message(ws, message_dict):
+    #data = json.dumps(message_dict).encode()
+    #print("sendMessage", data)
+    #ws.send(data)
+    ws.send("{'event':'addChannel','channel':'ok_sub_spot_bch_btc_ticker','binary':'0'}")
 
-def on_message(self, evt):
-    data = inflate(evt)  # data decompress
-    print(data)
-
-def inflate(data):
-    decompress = zlib.decompressobj(
-        -zlib.MAX_WBITS  # see above
-    )
-    inflated = decompress.decompress(data)
-    inflated += decompress.flush()
-    return inflated
+def on_message(ws, message):
+    print("on_message")
+    print(message)
 
 
-def on_error(self, evt):
-    print(evt)
+def on_error(ws, error):
+    print("Error: " + str(error))
+    error = gzip.decompress(error).decode()
+    print(error)
 
 
-def on_close(self, evt):
-    print('DISCONNECT')
+def on_close(ws):
+    print("### closed ###")
 
+def on_open(ws):
+    def run(*args):
+        # # 每2秒请求一次K线图，请求5次
+        while(True):
+            time.sleep(2)
+            ws.send("{'event':'addChannel','channel':'ok_sub_spot_bch_btc_ticker','binary':'0'}")
+        ws.close()
+        print("thread terminating...")
 
+    t = threading.Thread(target=run, args=())
+    t.start()
+
+#主程序
 if __name__ == "__main__":
-    url = "wss://real.okcoin.com:10440/websocket/okcoinapi"  # if okcoin.cn  change url wss://real.okcoin.cn:10440/websocket/okcoinapi
     websocket.enableTrace(True)
-    ws = websocket.WebSocketApp(url,
-                                on_open=on_open,
-                                on_message=on_message,
-                                on_error=on_error,
-                                on_close=on_close)
+    ws = websocket.WebSocketApp(
+        "wss://real.okex.com:10441/websocket",
+        on_open=on_open,
+        on_message=on_message,
+        on_error=on_error,
+        on_close=on_close
+    )
     ws.run_forever()
