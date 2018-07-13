ce402d401457207a7c65a0957e404d8b6ab0d3e2
huan-wang5 <huan-wang5@hnair.com>
2018-07-13 15:15:38 +0800

    usdt-eth

---
+++ coinWebsocket.py
@@ -0,0 +1,62 @@
+import gzip
+import json
+import threading
+
+import websocket
+import redis
+import time
+
+#OKEN币ws地址
+oken_ws_url = "wss://real.okex.com:10441/websocket"
+
+#创建数据库连接池
+def create_pool():
+    pool = redis.ConnectionPool(host="127.0.0.1", port=6379, db=0)
+    global __redis
+    __redis = redis.Redis(connection_pool=pool)
+    global __pipe
+    __pipe = __redis.pipeline(transaction=True)
+    print(__redis, __pipe)
+
+#OKEN币接收Usdt和Btc变更
+def on_message(ws, message):
+    '''__redis.set('oken-usdt-btc', json.loads(message)[0]["data"])
+    __pipe.execute()'''
+    print("接收消息" + ws)
+    print("message" + message)
+
+
+def on_error(ws, error):
+    print("OKEN币USDT-BTC Error: " + str(error))
+
+def on_open(ws):
+    def run(*args):
+        # # 每2秒请求一次K线图，请求5次
+        while(True):
+            time.sleep(2)
+            ws.send("{'event':'addChannel','channel':'ok_sub_spot_btc_usdt_ticker','binary':'0'}")
+            ws.send("{'event':'addChannel','channel':'ok_sub_spot_eth_usdt_ticker','binary':'0'}")
+        ws.close()
+        print("thread terminating...")
+
+    t = threading.Thread(target=run, args=())
+    t.start()
+
+def on_close(ws):
+    print("oken ### closed ###")
+    #runOkenWs()
+
+def runOkenWs():
+    websocket.enableTrace(True)
+    ws = websocket.WebSocketApp(
+        oken_ws_url,
+        on_open=on_open,
+        on_message=on_message(),
+        on_error=on_error(),
+        on_close=on_close()
+    )
+    ws.run_forever()
+#主程序
+if __name__ == "__main__":
+    create_pool()
+    runOkenWs()
