7285b37e1524b793903f9184a4f96b00481d7192
huan-wang5 <huan-wang5@hnair.com>
2018-07-13 15:17:22 +0800

    usdt-eth

--- coinWebsocket.py
+++ coinWebsocket.py
@@ -51,9 +51,9 @@ def runOkenWs():
     ws = websocket.WebSocketApp(
         oken_ws_url,
         on_open=on_open,
-        on_message=on_message(),
-        on_error=on_error(),
-        on_close=on_close()
+        on_message=on_message,
+        on_error=on_error,
+        on_close=on_close
     )
     ws.run_forever()
 #主程序
