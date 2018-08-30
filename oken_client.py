b4ef23bc5fadc0ee294c54fca62c10011a8cff95
379351549@qq.com <379351549@qq.com>
2018-08-30 19:32:09 +0800

    111

---
+++ oken_client.py
@@ -0,0 +1,14 @@
+import websockets
+import asyncio
+
+
+async def hello():
+    async with websockets.connect('ws://35.236.186.12:8765') as websocket:
+        name = input("what's your name?")
+        await websocket.send(name)
+        print(f"send server:{name}")
+        greeting = await websocket.recv()
+        print(f"receive from server:{greeting}")
+
+
+asyncio.get_event_loop().run_until_complete(hello())
