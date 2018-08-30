06b4d428bf88598412c16ddd0c56db1a09abc8b9
379351549@qq.com <379351549@qq.com>
2018-08-30 20:04:34 +0800

    111

--- oken_trade.py
+++ oken_trade.py
@@ -1,5 +1,6 @@
 import requests
 import json
+from datetime import datetime
 
 headers = {
     "authorization":"eyJhbGciOiJIUzUxMiJ9.eyJqdGkiOiJleDExMDE1MzU1NTA4MTYwNTJEMEY2MjFFQTlBRjA5NkIxSkxBViIsInVpZCI6IkZBUEU0SWxPYlF5c1haWW95dzBIUHc9PSIsInN0YSI6MCwibWlkIjowLCJpYXQiOjE1MzU1NTA4MTYsImV4cCI6MTUzNjE1NTYxNiwiYmlkIjowLCJkb20iOiJ3d3cub2tleC5jb20iLCJpc3MiOiJva2NvaW4ifQ.JddmytO3Hdkrgj4E5Xwe1fT2Zo9A--R5zsZnuMbksyPBKwNZLnrCF59fdGqxUsYaeckRwJScZYwFkePwr3OKqA"
@@ -8,5 +9,4 @@ url = "https://www.okex.com/v2/c2c-open/tradingOrders/group?digitalCurrencySymbo
 
 while(True):
     r = requests.get(url, headers=headers, timeout=10)
-    print(r.status_code)
-    print(json.loads(r.text)["data"]["buyTradingOrders"][0]["exchangeRate"])
+    print(datetime.now(),json.loads(r.text)["data"]["buyTradingOrders"][0]["exchangeRate"])
